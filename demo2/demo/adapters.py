5475970a92f638695374b303f8e3d7a5e09e302f
takapdayon <business.takap@gmail.com>
2025-01-21 01:56:27 +0900

    custom adopter

---
+++ demo2/demo/adapters.py
@@ -0,0 +1,9 @@
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
